c18f0972ba85a84100b5d4299fdf6ab5c594ee0e
Armine13 <arminevardazaryan@gmail.com>
2016-04-26 23:26:48 +0200

    reading dicom + canny filter

--- src/readFiles.py
+++ src/readFiles.py
@@ -4,12 +4,17 @@ Spyder Editor
 
 This is a temporary script file.
 """
-import pylab
 import dicom
 import os
 import numpy as np
-from matplotlib import pyplot, cm
+from matplotlib import cm
 
+import matplotlib.pyplot as plt
+from scipy import ndimage as ndi
+
+from skimage import feature
+from skimage import filters
+from skimage import io
 
 #ds = dicom.read_file("3D_T2/Image00001")
 #ds2 =dicom.read_file("3D_T2/Image00002")
@@ -28,6 +33,21 @@ for dirName, subdirList, fileList in os.walk(PathDicom):
         path = os.path.join(dirName,filename)
 #        lstFilesDCM.append(path) # List of file names+path
         DS.append(dicom.read_file(path))
-        
-pylab.imshow(DS[0].pixel_array, cmap=pylab.cm.bone)
-pylab.show()
+N = i #number of images
+
+#Display 17th image
+k = 17
+im = DS[k].pixel_array
+plt.figure()
+plt.imshow(im, cmap=pylab.cm.bone)
+plt.show()
+
+#Canny edge detection
+
+edges1 = feature.canny(im, sigma=1.0, low_threshold=10, high_threshold=100)
+
+plt.figure()
+plt.imshow(edges1, cmap=pylab.cm.bone)
+plt.show()
+
+io.imsave("prostate.png", im*255)
