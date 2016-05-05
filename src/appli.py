42a49782033301b45a4a48c4c36f3b582306a857
yanik-porto <yannick_porto@etu.u-bourgogne.fr>
2016-05-05 11:45:26 +0200

    Appli with click on image possibility

--- src/appli.py
+++ src/appli.py
@@ -20,13 +20,15 @@ import numpy as np
 from skimage import img_as_float
 from skimage import io
 
-"""Create a class
-"""
+"""function to be called when mouse is clicked"""
+def printcoords(event):
+    #outputting x and y coords to console
+    print (event.x,event.y)
+        
+"""Create a class"""
 class Interface(Frame):
     
-    """class which is derived from a window
-    """
-    
+    """class which is derived from a frame"""
     def __init__(self, fenetre, **kwargs):
         Frame.__init__(self, fenetre, width=768, height=576, **kwargs)
         self.pack(fill=BOTH)
@@ -35,21 +37,19 @@ class Interface(Frame):
         #Build widgets
         self.message = Label(self, text="You didn't click on the button")
         self.message.pack()
-        
-        self.button_exit = Button(self, text="Exit", command=self.quit)
-        self.button_exit.pack(side="left")
-        
+                
         self.button_click = Button(self, text="Click center", fg="red",
                                    command=self.cliquer)
         self.button_click.pack(side="right")
+        
+        self.button_stop = Button(self, text="Stop click", command=self.stop_click)
+        self.button_stop.pack(side="right")
         
         self.button_open = Button(self, text="Open", fg="blue", 
                                   command=self.openImg)
         self.button_open.pack(side="left")
         
-        self.button_stop = Button(self, text="Stop", command=self.stop_click)
-        self.button_stop.pack(side="right")
-        
+       
         self.frame = Frame(self, bd=2, relief=SUNKEN)
         self.frame.grid_rowconfigure(0, weight=1)
         self.frame.grid_columnconfigure(0, weight=1)
@@ -63,17 +63,10 @@ class Interface(Frame):
         self.yscroll.config(command=self.canvas.yview)
         self.frame.pack(fill=BOTH, expand=1)
 
-    
-        #function to be called when mouse is clicked
-    def printcoords(event):
-        #outputting x and y coords to console
-        print (event.x,event.y)
         
     def cliquer(self):
         """there was a click on the button
         we change the value  of the label message"""
-#        self.nb_clic += 1
-#        self.message["text"] = "You have clicked {} times".format(self.nb_clic)
         #mouseclick event
         self.canvas.bind("<Button 1>", printcoords)
         
@@ -82,32 +75,12 @@ class Interface(Frame):
         
         
     def openImg(self):
+        #open image and display in canvas
         File = askopenfilename(parent=fenetre, initialdir="C:/",title='Choose an image.')
         self.img = ImageTk.PhotoImage(Image.open(File))
         self.canvas.create_image(0,0,image=self.img,anchor="nw")
         self.canvas.config(scrollregion=self.canvas.bbox(ALL))
     
-#%%
-"""
-Read Discom images
-"""
-#DS = [] #list of all images
-#PathDicom = "../3D_T2/"
-#lstFilesDCM = []  # create an empty list
-#for dirName, subdirList, fileList in os.walk(PathDicom):
-#    for (i, filename) in enumerate(fileList):
-#        path = os.path.join(dirName,filename)
-##        lstFilesDCM.append(path) # List of file names+path
-#        DS.append(dicom.read_file(path))
-#
-#"""
-#Display 17th image
-#"""
-#k = 17
-#im = DS[k].pixel_array
-#im = img_as_float(im)
-#io.imsave("prostate3.png", im)
-
 
 #%%
 #Instantiate window
@@ -115,4 +88,4 @@ fenetre = Tk()
 interface = Interface(fenetre)
 interface.mainloop()
 #interface.destroy()
-fenetre.destroy()
+#fenetre.destroy()
