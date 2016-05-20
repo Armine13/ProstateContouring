9756dce0cec46bdce85f32b774e11a6594474010
Armine Vardazaryan <arminevardazaryan@gmail.com>
2016-05-20 21:59:07 +0200

    Update prostateContouring.py

--- src/prostateContouring.py
+++ src/prostateContouring.py
@@ -21,6 +21,8 @@ Created on Tue May 10 22:39:10 2016
 import numpy as np
 from skimage import measure
 from skimage import feature
+from skimage.filters import gaussian
+from skimage.segmentation import active_contour
 import scipy, scipy.ndimage, scipy.interpolate, numpy.fft, math
 
 class ProstateContouring:
@@ -206,7 +208,7 @@ class ProstateContouring:
                     self.image[int(np.round(self.yCenter-self.radii[i-4]*np.sin(self.angles[i-4]))), int(np.round(self.xCenter+self.radii[i-4]*np.cos(self.angles[i-4])))] = 1
         return self.image
 
-     def createContour(self, withSnake = False):
+    def createContour(self, withSnake = False):
         y, x = numpy.nonzero(self.image)
 
         C = (x - self.xCenter) + 1j * (y - self.yCenter)
@@ -380,3 +382,32 @@ def skeletonOrientation(skel):
         Orientations[row[ii], col[ii]] = rp[0].orientation
     return Orientations
     #------------------------------------------------------------------------------
+def line(x0, y0, x1, y1):
+        "Bresenham's line algorithm"
+        points_in_line = []
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+        x, y = x0, y0
+        sx = -1 if x0 > x1 else 1
+        sy = -1 if y0 > y1 else 1
+        if dx > dy:
+            err = dx / 2.0
+            while x != x1:
+                points_in_line.append((x, y))
+                err -= dy
+                if err < 0:
+                    y += sy
+                    err += dx
+                x += sx
+        else:
+            err = dy / 2.0
+            while y != y1:
+                points_in_line.append((x, y))
+                err -= dx
+                if err < 0:
+                    x += sx
+                    err += dy
+                y += sy
+        points_in_line.append((x, y))
+        return points_in_line
+
